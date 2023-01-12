0799e5aa354307b7341961b5af5ce756f63641ad
mohamed saber <mohamedahmed315@hotmail.com>
2023-01-12 18:01:50 +0200

    update

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,16 @@
 import "../css/styles.scss";
 
+// Dark mode
 let dark_mode = document.querySelector('#dark-mode-switch');
 
 dark_mode.addEventListener('change', function () {
     if (dark_mode.checked == true) {
         document.querySelector('body').classList.add('dark-mode');
+        document.querySelector('body').classList.remove('light-mode');
     }
 
-    else document.querySelector('body').classList.remove('dark-mode');
-
-});
-
-$("h1").on("click", function(){
-    alert("clicked1");
+    else {
+        document.querySelector('body').classList.remove('dark-mode');
+        document.querySelector('body').classList.add('light-mode');
+    }
 });
